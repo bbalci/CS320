a9759a3f41a495e0c0a8e682dc1bb91ff5f03239
bsunal <begum.sunal@ozu.edu.tr>
2016-12-13 00:27:43 +0200

    Add files via upload

---
+++ RACMS/databaseLayer/Car.java
@@ -0,0 +1,188 @@
+package databaseLayer;
+
+import java.util.Date;
+
+public class Car {
+	private String plate;
+	private int vendorNo;
+	private String brand;
+	private String model;
+	private String photo;
+	private String gearType;
+	private String fuelType;
+	private double dailyPrice;
+	private int minRentAge;
+	private String segment;
+	private String notes;
+	private double km;
+	private String status;
+	private Date outDate;
+	private Date returnDate;
+	private String renterName;
+	private int renterID;
+	private String renterAddress;
+	private int renterPhone;
+	private String renterEmail;
+
+	// Getters
+	public String getPlate() {
+		return plate;
+	}
+
+	public int getVendorNo() {
+		return vendorNo;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public String getGearType() {
+		return gearType;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public double getDailyPrice() {
+		return dailyPrice;
+	}
+
+	public int getMinRentAge() {
+		return minRentAge;
+	}
+
+	public String getSegment() {
+		return segment;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public double getKm() {
+		return km;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public Date getOutDate() {
+		return outDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public String getRenterName() {
+		return renterName;
+	}
+
+	public int getRenterID() {
+		return renterID;
+	}
+
+	public String getRenterAddress() {
+		return renterAddress;
+	}
+
+	public int getRenterPhone() {
+		return renterPhone;
+	}
+
+	public String getRenterEmail() {
+		return renterEmail;
+	}
+
+	// Setters
+	public void setPlate(String newPlate) {
+		plate = newPlate;
+	}
+
+	public void setVendorNo(int newVendorNo) {
+		vendorNo = newVendorNo;
+	}
+
+	public void setBrand(String newBrand) {
+		brand = newBrand;
+	}
+	
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+
+	public void setPhoto(String newPhoto) {
+		photo = newPhoto;
+	}
+
+	public void setGearType(String newGearType) {
+		gearType = newGearType;
+	}
+
+	public void setFuelType(String newFuelType) {
+		fuelType = newFuelType;
+	}
+
+	public void setDailyPrice(double newDailyPrice) {
+		dailyPrice = newDailyPrice;
+	}
+
+	public void setMinRentAge(int newMinRentAge) {
+		minRentAge = newMinRentAge;
+	}
+
+	public void setSegment(String newSegment) {
+		segment = newSegment;
+	}
+
+	public void setNotes(String newNotes) {
+		notes = newNotes;
+	}
+
+	public void setKm(int newKm) {
+		km = newKm;
+	}
+
+	public void setStatus(String newStatus) {
+		status = newStatus;
+	}
+
+	public void setOutDate(Date newOutDate) {
+		outDate = newOutDate;
+	}
+
+	public void setReturnDate(Date newReturnDate) {
+		returnDate = newReturnDate;
+	}
+
+	public void setRenterName(String newRenterName) {
+		renterName = newRenterName;
+	}
+
+	public void setRenterID(int newRenterID) {
+		renterID = newRenterID;
+	}
+
+	public void setRenterAddress(String newRenterAddress) {
+		renterAddress = newRenterAddress;
+	}
+
+	public void setRenterPhone(int newRenterPhone) {
+		renterPhone = newRenterPhone;
+	}
+
+	public void setRenterEmail(String newRenterEmail) {
+		renterEmail = newRenterEmail;
+	}
+}
---
+++ RACMS/databaseLayer/DatabaseManager.java
@@ -0,0 +1,194 @@
+package databaseLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class DatabaseManager {
+	static Scanner scanner = new Scanner(System.in);
+
+	static final String DB_URL = "jdbc:mysql://localhost:3306/racms";
+	static final String USER = "root";
+	static final String PASS = "password";
+
+	static Connection conn = null;
+	static Statement stmt = null;
+	
+	public DatabaseManager() {
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+		} catch (SQLException ex) {
+			System.err.println("Could not get an sql connection.");
+			System.err.println(ex.getErrorCode() + ex.getMessage());
+			System.exit(0);
+		} finally {
+			if (conn != null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			if (stmt != null)
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+	
+	public ArrayList<Car> getCarList(){
+		ArrayList<Car> cars = new ArrayList<Car>();
+		
+		String query = "Select * from car";
+		try (ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				Car car = new Car();
+				car.setBrand(rs.getString("brand"));
+				car.setDailyPrice(rs.getDouble("Daily_Price"));
+				car.setFuelType(rs.getString("Fuel_Type"));
+				car.setGearType(rs.getString("Gear_Type"));
+				car.setKm(rs.getInt("km"));
+				car.setMinRentAge(rs.getInt("Min_rent_age"));
+				car.setModel(rs.getString("model"));
+				car.setNotes(rs.getString("notes"));
+				car.setOutDate(rs.getDate("out_date"));
+				car.setPhoto(rs.getString("photo"));
+				car.setPlate(rs.getString("plate"));
+				car.setRenterAddress(rs.getString("renter_address"));
+				car.setRenterEmail(rs.getString("renter_email"));
+				car.setRenterID(rs.getInt("renter_id"));
+				car.setRenterName(rs.getString("renter_name"));
+				car.setRenterPhone(rs.getInt("renter_phone"));
+				car.setReturnDate(rs.getDate("return_date"));
+				car.setSegment(rs.getString("segment"));
+				car.setStatus(rs.getString("status"));
+				car.setVendorNo(rs.getInt("vendor_no"));
+				cars.add(car);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return cars;
+	}
+	
+	public ArrayList<Admin> getAdminList() {
+		ArrayList<Admin> admins = new ArrayList<Admin>();
+		
+		String query = "Select * from admin";
+		try (ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				Admin admin = new Admin();
+				admin.setPasswd(rs.getString("passwd"));
+				admin.setVendorName(rs.getString("vendor_name"));
+				admin.setVendorNo(rs.getInt("vendor_no"));
+				admins.add(admin);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return admins;
+	}
+	
+	public boolean checkAdmin(int vendor_no, String vendor_name, String passwd){
+		Admin admin = new Admin();
+		admin.setPasswd(null);
+		admin.setVendorName(null);
+		
+		String query = "Select * from admin where vendor_no = '"+vendor_no + "' vendor_name= '"+ vendor_name + "' passwd = '"+ passwd +"'";
+		try (ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				admin.setPasswd(rs.getString("passwd"));
+				admin.setVendorName(rs.getString("vendor_name"));
+				admin.setVendorNo(rs.getInt("vendor_no"));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		if(admin.getPasswd().equals(null) && admin.getVendorName().equals(null))
+			return false;
+		else
+			return true;
+		
+	}
+	
+	public void addAdmin(int vendor_no, String vendor_name, String passwd) {
+		String query = "Insert into admin values(" +vendor_no +",'"+vendor_name +"','" +passwd +"')";
+		try {
+			stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public void addCar(String plate, int vendorNo, String brand, String model, String photo, 
+			String gearType, String fuelType, double dailyPrice, int minRentAge, String segment, 
+			String notes, double km, String status, Date outDate, Date returnDate, String renterName, 
+			int renterID, String renterAddress, int renterPhone, String renterEmail) {
+		
+		String query = "insert into car values('"+plate+"',"+vendorNo+",'"+brand+"','"+model+"','"+
+			photo+"','"+gearType+"','"+fuelType+"',"+dailyPrice+","+minRentAge+",'"+segment+"','"+
+			notes+"',"+km+",'"+status+","+outDate+","+returnDate+",'"+renterName+"',"+renterID+",'"+
+			renterAddress+"',"+renterPhone+",'"+renterEmail+"')";
+		
+		try {
+			stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public boolean checkAvailable(String plate) {
+		String query = "Select status from car where plate = " + plate;
+		String status = null;
+		
+		try (ResultSet rs = stmt.executeQuery(query)){
+			while(rs.next()){
+				status = rs.getString("status");
+			}
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		if(status.equalsIgnoreCase("available"))
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean checkAvailableAtCertainDates(String plate, Date rentingDate, Date returningDate) {
+		if(checkAvailable(plate)) 
+			return true;
+		else {
+			String query = "Select out_date, return_date from car where plate = " + plate;
+			Date outDate = null;
+			Date returnDate = null;
+			
+			try (ResultSet rs = stmt.executeQuery(query)){
+				while(rs.next()){
+					outDate = rs.getDate("out_date");
+					returnDate = rs.getDate("return_date");
+				}
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			
+			if(rentingDate.after(returnDate))
+				return true;
+			else if(returningDate.before(outDate))
+				return true;
+			else
+				return false;
+		}
+	}
+	
+}
